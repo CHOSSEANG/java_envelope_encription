58e0593d182a1895592fb9aa4c972a28bbc53ac3
ssang-labsd <ssang@labsd.net>
2025-04-17 20:30:37 +0900

    feat : 암호화 복호화 테스트 코드 구현

---
+++ src/test/java/com/chosseang/EncryptDecryptTest.java
@@ -0,0 +1,21 @@
+package com.chosseang;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EncryptDecryptTest {
+
+	@Test
+	public void encryptDecryptTest(){
+
+		String input = "test";
+
+		EncryptResult encryptResult = Encrypt.encrypt(input);
+
+		DecryptRequest decryptRequst = new DecryptRequest(encryptResult.getEncryptedData(),encryptResult.getIv(),encryptResult.getDataKey());
+
+		String decryptResult = Encrypt.decrypt(decryptRequst);
+
+		Assertions.assertEquals(input,decryptResult);
+	}
+}
