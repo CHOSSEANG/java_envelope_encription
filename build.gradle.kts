c9933b6368ed380509ee93b856a89b3eeb885836
ssang-labsd <ssang@labsd.net>
2025-04-19 10:17:20 +0900

    chore : AWS KMS 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.amazonaws:aws-java-sdk-core:1.12.711")
+    implementation("com.amazonaws:aws-java-sdk-kms:1.12.711")
+
 }
 
 tasks.test {
